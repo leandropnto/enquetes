293d4aef3fedfd39d52e3caaf201ba5a97ac6d4e
Leandro Pinto <leandro.pnto@gmail.com>
2020-11-05 17:34:27 -0300

    ensure LoginPage calls validate when form changes

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -1,10 +1,21 @@
-import 'package:enquetes/ui/pages/login_page.dart';
+import 'package:enquetes/ui/pages/login/login_page.dart';
+import 'package:enquetes/ui/pages/pages.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class LoginPresenterSpy extends Mock implements LoginPresenter {}
 
 void main() {
+  LoginPresenter presenter;
+
   Future<void> loadPage(WidgetTester tester) async {
-    final loginPage = MaterialApp(home: LoginPage());
+    presenter = LoginPresenterSpy();
+    final loginPage = MaterialApp(
+        home: LoginPage(
+      presenter: presenter,
+    ));
     await tester.pumpWidget(loginPage);
   }
 
@@ -39,5 +50,19 @@ void main() {
 
     final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
     expect(button.onPressed, null);
+  });
+
+  testWidgets('Should call validate with correct values',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+    final email = faker.internet.email();
+    await tester.enterText(find.bySemanticsLabel('Email'), email);
+
+    verify(presenter.validateEmail(email));
+
+    final password = faker.internet.password();
+    await tester.enterText(find.bySemanticsLabel('Senha'), password);
+
+    verify(presenter.validatePassword(password));
   });
 }

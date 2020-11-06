6f1351a3bac8ab6ebeaf4f0750b1ea9a7d73e6b7
Leandro Pinto <leandro.pnto@gmail.com>
2020-11-06 18:53:10 -0300

    ensure EmailErrorStream not emit duplicated event

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -35,7 +35,10 @@ void main() {
     test('Should emit email error if validation fails', () {
       mockValidation(value: "error");
 
+      sut.emailErrorStream
+          .listen(expectAsync1((error) => expect(error, 'error')));
       expectLater(sut.emailErrorStream, emits('error'));
+      sut.validateEmail(email);
       sut.validateEmail(email);
     });
   });

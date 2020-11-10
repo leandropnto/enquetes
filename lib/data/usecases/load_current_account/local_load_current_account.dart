ba6743c43aae2b7e925a8a3c5e5b164fedc9b2ea
Leandro Pinto <leandro.pnto@gmail.com>
2020-11-10 18:09:24 -0300

    Move production code to their own files.

---
+++ lib/data/usecases/load_current_account/local_load_current_account.dart
@@ -0,0 +1,19 @@
+import 'package:enquetes/data/cache/cache.dart';
+import 'package:enquetes/domain/entities/entities.dart';
+import 'package:enquetes/domain/helpers/helpers.dart';
+import 'package:enquetes/domain/usecases/usecases.dart';
+
+class LocalLoadCurrentAccount implements LoadCurrentAccount {
+  final FetchSecureCacheStorage fetchSecureCacheStorage;
+
+  LocalLoadCurrentAccount({this.fetchSecureCacheStorage});
+
+  Future<AccountEntity> load() async {
+    try {
+      final strToken = await fetchSecureCacheStorage.fetchSecure('token');
+      return AccountEntity(strToken);
+    } catch (e) {
+      throw DomainError.unexpected;
+    }
+  }
+}
--- test/data/usecases/load_current_account/local_load_current_account_test.dart
+++ test/data/usecases/load_current_account/local_load_current_account_test.dart
@@ -1,31 +1,13 @@
+import 'package:enquetes/data/cache/cache.dart';
+import 'package:enquetes/data/usecases/usecases.dart';
 import 'package:enquetes/domain/entities/entities.dart';
 import 'package:enquetes/domain/helpers/domain_error.dart';
-import 'package:enquetes/domain/usecases/load_current_account.dart';
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-abstract class FetchSecureCacheStorage {
-  Future<String> fetchSecure(String key);
-}
-
 class FetchSecureCacheStorageSpy extends Mock
     implements FetchSecureCacheStorage {}
-
-class LocalLoadCurrentAccount implements LoadCurrentAccount {
-  final FetchSecureCacheStorage fetchSecureCacheStorage;
-
-  LocalLoadCurrentAccount({this.fetchSecureCacheStorage});
-
-  Future<AccountEntity> load() async {
-    try {
-      final strToken = await fetchSecureCacheStorage.fetchSecure('token');
-      return AccountEntity(strToken);
-    } catch (e) {
-      throw DomainError.unexpected;
-    }
-  }
-}
 
 void main() {
   //Variables

d691e925f351218da0b94ab77dd201029e06699e
Leandro Pinto <leandro.pnto@gmail.com>
2020-11-12 00:05:00 -0300

    Move splashPage to its own file

---
+++ lib/ui/pages/splash/splash_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'splash_presenter.dart';
+
+class SplashPage extends StatelessWidget {
+  final SplashPresenter presenter;
+
+  const SplashPage({Key key, @required this.presenter}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('4Dev'),
+      ),
+      body: Builder(
+        builder: (context) {
+          presenter.navigateToStream.listen((route) =>
+              route?.isNotEmpty == true ? Get.offAllNamed(route) : () {});
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
--- test/ui/pages/splash_page_test.dart
+++ test/ui/pages/splash_page_test.dart
@@ -1,45 +1,37 @@
+import 'dart:async';
+
+import 'package:enquetes/ui/pages/pages.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 
-abstract class SplashPresenter {
-  Future<void> loadCurrentAccount();
-}
-
 class SplashPagePresenterSpy extends Mock implements SplashPresenter {}
-
-class SplashPage extends StatelessWidget {
-  final SplashPresenter presenter;
-
-  const SplashPage({Key key, @required this.presenter}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    presenter.loadCurrentAccount();
-
-    return Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
-    );
-  }
-}
 
 void main() {
   //Variables
 
   SplashPresenter presenter;
+  StreamController navigateToController;
 
   Future<void> loadPage(WidgetTester tester) async {
     presenter = SplashPagePresenterSpy();
+    navigateToController = StreamController<String>();
+
+    when(presenter.navigateToStream)
+        .thenAnswer((_) => navigateToController.stream);
 
     await tester.pumpWidget(
       GetMaterialApp(
         initialRoute: "/",
         getPages: [
           GetPage(name: "/", page: () => SplashPage(presenter: presenter)),
+          GetPage(
+            name: "/any_route",
+            page: () => Scaffold(
+              body: Text('fake page'),
+            ),
+          ),
         ],
       ),
     );
@@ -47,6 +39,10 @@ void main() {
   //Mocks
 
   //helpers
+
+  tearDown(() {
+    navigateToController.close();
+  });
 
   testWidgets('Should present loading on page load',
       (WidgetTester tester) async {
@@ -59,5 +55,27 @@ void main() {
     await loadPage(tester);
 
     verify(presenter.loadCurrentAccount()).called(1);
+  });
+
+  testWidgets('Should  load page ', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    navigateToController.add('/any_route');
+    await tester.pumpAndSettle();
+
+    expect(Get.currentRoute, "/any_route");
+    expect(find.text('fake page'), findsOneWidget);
+  });
+
+  testWidgets('Should not load page ', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    navigateToController.add('');
+    await tester.pump();
+    expect(Get.currentRoute, "/");
+
+    navigateToController.add(null);
+    await tester.pump();
+    expect(Get.currentRoute, "/");
   });
 }

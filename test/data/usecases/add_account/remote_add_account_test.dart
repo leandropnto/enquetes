468eb338c381d3167b68a61301aa615f73963577
Leandro Pinto <leandro.pnto@gmail.com>
2020-11-13 23:55:18 -0300

    Ensure RemoteAddAccount call httpClient with correct values

---
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -0,0 +1,58 @@
+import 'package:enquetes/data/http/http.dart';
+import 'package:enquetes/data/usecases/usecases.dart';
+import 'package:enquetes/domain/usecases/usecases.dart';
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class HttpClientSpy extends Mock implements HttpClient {}
+
+void main() {
+  RemoteAddAccount sut;
+  HttpClientSpy httpClient;
+  String url;
+  AddAccountParams params;
+  String password;
+
+  PostExpectation mockRequest() => when(
+        httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')),
+      );
+
+  void mockHttpData(Map data) {
+    mockRequest().thenAnswer((_) async => data);
+  }
+
+  void mockHttpError(HttpError error) {
+    mockRequest().thenThrow(error);
+  }
+
+  Map mockValidData() =>
+      {'accessToken': faker.guid.guid(), 'name': faker.person.name()};
+
+  setUp(() {
+    password = faker.internet.password();
+    httpClient = HttpClientSpy();
+    url = faker.internet.httpsUrl();
+    sut = RemoteAddAccount(httpClient: httpClient, url: url);
+    params = AddAccountParams(
+        name: faker.person.name(),
+        email: faker.internet.email(),
+        password: password,
+        passwordConfirmation: password);
+  });
+
+  test('Should call HttpClient with correct values', () async {
+    await sut.add(params);
+
+    verify(
+      httpClient.request(
+        url: url,
+        method: 'post',
+        body: RemoteAddAccountParams.fromDomain(params).toJson(),
+      ),
+    );
+  });
+}

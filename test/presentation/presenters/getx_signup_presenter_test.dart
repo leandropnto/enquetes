74f8dca93f3cfa46a38d3602412b5e8dd56654f3
Leandro Pinto <leandro.pnto@gmail.com>
2020-11-26 00:18:26 -0300

    ensure GetXSignUpPresenter emits correct events if  AddAccount success

--- test/presentation/presenters/getx_signup_presenter_test.dart
+++ test/presentation/presenters/getx_signup_presenter_test.dart
@@ -231,5 +231,18 @@ void main() {
 
       await sut.signUp();
     });
+
+    test('Should emit correct events  if AddAccount success', () async {
+      mockSaveCurrentAccountError();
+      sut.validateName(name);
+      sut.validateEmail(email);
+      sut.validatePassword(password);
+      sut.validatePasswordConfirmation(passwordConfirmation);
+
+      expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+      sut.mainErrorStream.listen(expectAsync1((error) => expect(error, UIError.unexpected)));
+
+      await sut.signUp();
+    });
   });
 }
